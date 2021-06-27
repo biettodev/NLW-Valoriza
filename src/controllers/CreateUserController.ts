d42f085acd9ca6bc8f9b81148e62a951966d3615
Gabriel Carrilho <gabrielnascimento0713@gmail.com>
2021-06-27 15:29:51 -0300

    Commit das alterações da terceira aula

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,13 +3,15 @@ import { CreateUserService } from "../services/CreateUserService"
 
 class CreateUserController {
 	async handle(req: Request, res: Response){
-		const { name, email, admin } = req.body
 		
+		const { name, email, admin } = req.body
+	
 		const createUserService = new CreateUserService()
 	
-		const user = createUserService.execute({name, email, admin})
+		const user = await createUserService.execute({name, email, admin})
 		
 		return res.json(user)
+		
 	}
 }
 
---
+++ src/database/migrations/1624809062006-CreateTags.ts
@@ -0,0 +1,38 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTags1624809062006 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "tags",
+				columns: [
+					{
+						name: "id",
+						type: "uuid",
+						isPrimary: true
+					},
+					{
+						name: "name",
+						type: "varchar"
+					},
+					{
+						name: "created_at",
+						type: "timespamp",
+						default: "now()"
+					},
+					{
+						name: "updated_at",
+						type: "timestamp",
+						default: "now()"
+					}
+				]
+			})
+		)
+	}
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("tags")
+	}
+
+}
